49c65ff7acdcc7ff941994de3f1bb35d631ea385
aqeel <maqeel191@gmail.com>
2025-06-21 14:29:23 -0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import colors from './src/theme/colors';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+       <StatusBar
+                barStyle="light-content"
+                backgroundColor={colors.greenPrimary}
+              />
+     
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
